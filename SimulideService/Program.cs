c8366af92c89dd74c5236fb87683997ff6da2e39
Dalton DeMaria <ddemaria.career@gmail.com>
2025-12-26 17:44:43 -0600

    fix docker compose to get rid of error spam

--- SimulideService/Program.cs
+++ SimulideService/Program.cs
@@ -57,7 +57,7 @@ if (builder.Environment.IsDevelopment())
         options.AddPolicy("AllowLocalhost", builder =>
         {
             builder
-            .WithOrigins("http://127.0.0.1:5173")
+            .WithOrigins("http://127.0.0.1:5173", "http://localhost:5173")
             .AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials();
